c198b735eb5ed6c9c9afe7f98e63a0462d935f6b
Student <student@localhost>
2026-07-01 08:59:03 +0700

    ส่งงานในห้อง

--- workshop2_5.ts
+++ workshop2_5.ts
@@ -1,9 +1,3 @@
-let products: [string, number, number][] = [
-    ["Laptop", 1500, 5],
-    ["Mouse", 25, 50],
-    ["Keyboard", 45, 30]
-];
+import { printGradeReport, students } from "./gradeManager";
 
-for (let product of products) {
-    console.log(`Product: ${product[0]}, Price: $${product[1]}, Quantity: ${product[2]}`);
-}
+printGradeReport(students);
